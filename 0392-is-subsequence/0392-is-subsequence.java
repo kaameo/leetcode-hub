8ee6286c883c7560a5ab51a55e93859cd3d35256
Jinsu Hong <kaameo12@gmail.com>
2025-10-19 01:05:45 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length() == 0){
+            return true;
+        }
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+        int sp = 0;
+        int tp = 0;
+        boolean answer = false;
+
+        while (tp < tArr.length) {
+            if (tArr[tp] == sArr[sp]) {
+                sp++;
+                tp++;
+            } else {
+                tp++;
+            }
+
+            if (sp >= sArr.length) {
+                answer = true;
+            }
+        }
+
+        return answer;
+    }
+    // time complexity O(m) m is t.length
+    // space complexity O(n+m)
+}
