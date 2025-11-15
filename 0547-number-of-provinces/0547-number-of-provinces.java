f802869f3fc1ce8117a15e08650674d2288104f6
Jinsu Hong <kaameo12@gmail.com>
2025-11-16 02:04:00 +0900

    Time: 2 ms (47.71%), Space: 47.2 MB (78.95%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                provinces++;
+
+                Deque<Integer> deque = new ArrayDeque<>();
+                deque.push(i);
+                visited[i] = true;
+
+                while (!deque.isEmpty()) {
+                    int node = deque.pop();
+
+                    for (int j = 0; j < n; j++) {
+                        if (isConnected[node][j] == 1 && !visited[j]) {
+                            visited[j] = true;
+                            deque.push(j);
+                        }
+                    }
+                }
+            }
+        }
+        return provinces;
+    }
+    // time complexity O(n)
+    // space complexity O(n)
+}
