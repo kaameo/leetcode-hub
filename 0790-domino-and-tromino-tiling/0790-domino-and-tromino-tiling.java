be4e224d5a2d70839d5befca15d746f39929750e
Jinsu Hong <kaameo12@gmail.com>
2025-12-24 17:47:03 +0900

    Time: 0 ms (100%), Space: 42.1 MB (44.72%) - LeetHub

---
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.java
@@ -0,0 +1,18 @@
+class Solution {
+    static final long MOD = 1_000_000_007L;
+
+    public int numTilings(int n) {
+        long[] dp = new long[Math.max(n + 1, 3)];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
+        }
+        return (int) dp[n];
+    }
+    // time complexity O(n)
+    // space complexity O(n)
+}
